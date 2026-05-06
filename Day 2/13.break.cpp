2311bcafc79e1b805eeb934b8d363b4b56d32d91
anushka1330 <anushka97743@gmail.com>
2026-05-06 09:09:37 +0530

    Normal Commit

--- Day 2/13.break.cpp
+++ Day 2/13.break.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int num,sum=0;
+    while(true){
+        cout<<"Enter a number (0 to stop): ";
+        cin>>num;
+        if(num==0){
+            break;
+        }
+        sum+=num;
+    }
+    cout<<"Sum of entered numbers: "<<sum<<endl;
+    return 0;
 }
